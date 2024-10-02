e828c58df79db61b9d608c55469d866d973c7445
Adam Makowski <adam.makowski01@gmail.com>
2024-10-02 16:31:00 +0200

    Add menu button and share website button

--- browserApp/Views/HomeView.swift
+++ browserApp/Views/HomeView.swift
@@ -10,9 +10,11 @@ import WebKit
 
 struct HomeView: View {
     
-    @EnvironmentObject var browserViewModel: BrowserViewModel
+    @EnvironmentObject var vm: BrowserViewModel
     
     @FocusState var searchBarFocused: Bool
+    
+    @State var showOptions = false
     
     var body: some View {
         
@@ -22,15 +24,15 @@ struct HomeView: View {
             
             webView
             
-            .sheet(isPresented: $browserViewModel.showSettingsView, content: {
-                SheetView()
-                    .presentationDetents([.fraction(0.7), .fraction(0.99)])
-                    .presentationBackground(.thinMaterial)
-                    .presentationCornerRadius(30)
-            })
-            .onAppear{
-                browserViewModel.loadURL()
-            }
+                .sheet(isPresented: $vm.showSettingsView, content: {
+                    SheetView()
+                        .presentationDetents([.fraction(0.7), .fraction(0.99)])
+                        .presentationBackground(.thinMaterial)
+                        .presentationCornerRadius(30)
+                })
+                .onAppear{
+                    vm.loadURL()
+                }
         }
     }
 }
@@ -38,11 +40,11 @@ struct HomeView: View {
 extension HomeView {
     private var webView: some View {
         VStack {
-            browserViewModel.webView
+            vm.webView
                 .cornerRadius(8)
                 .overlay(alignment: .top, content: {
-                    if browserViewModel.isLoading{
-                        ProgressView(value: browserViewModel.loadingProgress, total: 1.0)
+                    if vm.isLoading{
+                        ProgressView(value: vm.loadingProgress, total: 1.0)
                             .progressViewStyle(LinearProgressViewStyle())
                             .accentColor(.blue)
                             .opacity(0.8)
@@ -56,60 +58,92 @@ extension HomeView {
     }
     
     private var bottomBar: some View {
-        ZStack {
-            Color(.clear)
-                .background(.thinMaterial)
-                .cornerRadius(50)
-                .shadow(color: .black.opacity(0.5), radius: 10, x: 0.0, y: 0.0)
+        
+        
+        ZStack(alignment: .bottomLeading) {
+            if showOptions{
+                MenuButtonView(
+                    showMenu: $vm.showSettingsView,
+                    showOptions: $showOptions,
+                    sharedURL: vm.websiteURL)
+                .frame(maxWidth: 0, alignment: .bottomLeading)
+                .zIndex(1.0)
+                .offset(x: 0, y: -65)
+                .padding()
+                .transition(.scale)
                 
+            }
+            ZStack{
                 
-            HStack {
-                Button(action: {
-                    browserViewModel.showSettingsView.toggle()
+                Color(.clear)
+                    .background(.thinMaterial)
+                    .cornerRadius(50)
+                    .shadow(color: .black.opacity(0.5), radius: 10, x: 0.0, y: 0.0)
+                
+                HStack {
                     
-                }, label: {
-                    Image(systemName: "globe")
-                        .font(.largeTitle)
-                        .foregroundColor(.black)
-                })
-                .padding(.leading)
-                
-                
-                TextField("\(Image(systemName: "magnifyingglass")) Search", text: searchBarFocused ? $browserViewModel.websiteURL : $browserViewModel.urlHost)
-                    .foregroundStyle(.primary)
-                    .font(.headline)
-                    .multilineTextAlignment(searchBarFocused ? .leading : .center)
-                    .focused($searchBarFocused)
-                    .padding(.horizontal)
-                    .padding(.vertical, 4)
-                    .background(
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(Color("textFieldBackgroundColor")))
-                    .shadow(radius: 10)
-                    .submitLabel(.search)
-                    .textInputAutocapitalization(.never)
-                    .disableAutocorrection(true)
-                    .onSubmit {
-                        browserViewModel.loadURL()
-                        searchBarFocused = false
-                    }
-                    .padding(.trailing, searchBarFocused ? 0 : 20)
-
-                if searchBarFocused{
-                    Button(action: {browserViewModel.websiteURL = ""}, label: {
-                        Image(systemName: "x.circle.fill")
-                            .foregroundStyle(.black)
+                    Button(action: {
+                        withAnimation {
+                            showOptions.toggle()
+                        }
+                        
+                        
+                    }, label: {
+                        Image(systemName: "arrow.up")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .fontDesign(.rounded)
+                            .foregroundColor(.primary)
+                            .padding(5)
+                            .background(
+                                Circle()
+                                    .fill(.thinMaterial)
+                                    .shadow(radius: 5)
+                            )
+                            .rotationEffect(Angle(
+                                degrees: showOptions ? -180 : 0))
                     })
-                    .transition(AnyTransition.asymmetric(insertion: .opacity.animation(.easeInOut(duration: 0.1).delay(0.25)), removal: .opacity.animation(.easeInOut(duration: 0.01))))
-                    .padding(.trailing)
+                    .padding(.leading)
+                    
+                    TextField("\(Image(systemName: "magnifyingglass")) Search", text: searchBarFocused ? $vm.websiteURL : $vm.urlHost)
+                        .foregroundStyle(.primary)
+                        .font(.headline)
+                        .multilineTextAlignment(searchBarFocused ? .leading : .center)
+                        .focused($searchBarFocused)
+                        .padding(.horizontal)
+                        .padding(.vertical, 4)
+                        .background(
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color("textFieldBackgroundColor")))
+                        .shadow(radius: 10)
+                        .submitLabel(.search)
+                        .textInputAutocapitalization(.never)
+                        .disableAutocorrection(true)
+                        .onSubmit {
+                            vm.loadURL()
+                            searchBarFocused = false
+                        }
+                        .padding(.trailing, searchBarFocused ? 0 : 20)
+                    
+                    if searchBarFocused{
+                        Button(action: {vm.websiteURL = ""}, label: {
+                            Image(systemName: "x.circle.fill")
+                                .foregroundStyle(.black)
+                        })
+                        .transition(AnyTransition.asymmetric(insertion: .opacity.animation(.easeInOut(duration: 0.1).delay(0.25)), removal: .opacity.animation(.easeInOut(duration: 0.01))))
+                        .padding(.trailing)
+                    }
                 }
             }
+            .padding(.vertical, 25)
+            .padding(.horizontal, 20)
+            .frame(height: 100)
+            
         }
         .animation(.easeInOut(duration: 0.3), value: searchBarFocused)
         .offset(y: searchBarFocused ? -330 : 0)
-        .padding(.vertical, 25)
-        .padding(.horizontal, 20)
-        .frame(height: 100)
+
+        
     }
 }
 
---
+++ browserApp/Views/MenuButtonView.swift
@@ -0,0 +1,62 @@
+//
+//  MenuButtonView.swift
+//  browserApp
+//
+//  Created by Adam Makowski on 02/10/2024.
+//
+
+import SwiftUI
+
+struct MenuButtonView: View {
+    @Binding var showMenu: Bool
+    @Binding var showOptions: Bool
+    var sharedURL: String
+    
+    var body: some View {
+            VStack(alignment: .leading, spacing: 15){
+                Button {
+                    showMenu = true
+                    showOptions = false
+                } label: {
+                    Text("Menu")
+                        .font(.headline)
+                        .fontDesign(.rounded)
+                        .tint(.primary)
+                        .padding()
+                        .padding(.horizontal)
+                        .frame(width: 160)
+                        .background(
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(.thinMaterial)
+                                .shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: 5)
+                        )
+                }
+                if let url = URL(string: sharedURL) {
+                    ShareLink(item: url){
+                        Label("Share", systemImage: "square.and.arrow.up")
+                    }
+                        .font(.headline)
+                        .fontDesign(.rounded)
+                        .tint(.primary)
+                        .padding()
+                        .padding(.horizontal)
+                        .frame(width: 160)
+                        .background(
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(.thinMaterial)
+                                .shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: 5)
+                        )
+                    
+                }
+            }
+            .padding()
+            .background(.thickMaterial)
+            .cornerRadius(15)
+            .shadow(radius: 5)
+
+    }
+}
+
+#Preview {
+    MenuButtonView(showMenu: .constant(true), showOptions: .constant(true), sharedURL: "www.google.pl")
+}
